70d6a70b45e7ff4341119c9e76296813c8ca37fc
Siva Arigela <siva.arigela@ibm.com>
2025-05-02 17:15:08 +0530

    updated eslint file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,10 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+module.exports = {
+  rules: {
+    'react/no-unescaped-entities': 'off',
+  },
+};
+
 export default eslintConfig;

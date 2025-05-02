d2c1217779d1b415700b7dddeb8a21b6aa26a0e6
Siva Arigela <siva.arigela@ibm.com>
2025-05-02 17:47:58 +0530

    Updated the data with new params

--- src/app/gallery/[id]/page.tsx
+++ src/app/gallery/[id]/page.tsx
@@ -8,11 +8,10 @@ interface Post {
   title: string
 }
 
-export default async function Page({
-  params,
-}: {
-  params: { id: string }
-}) {
+type Params = Promise<{ id: string }>
+
+export default async function Page(props: { params: Params }) {
+  const params = await props.params;
   const res = await fetch(`https://jsonplaceholder.typicode.com/photos/${params.id}`)
 
   if (!res.ok) {
@@ -22,32 +21,34 @@ export default async function Page({
   const post: Post = await res.json()
 
   return (
-    <div className="pt-[18px] flex justify-between">
-      <div className="w-2/3">
-        <h1 className="text-3xl font-bold mb-4 pl-2">{post.title}</h1>
-        <img
-          src={`https://picsum.photos/seed/${post.id}/300/200`}
-          alt={post.title}
-          className="w-full h-40 object-cover rounded float-left p-1.5 mb-4"
-        />
-        <p className="text-justify pl-2">Your paragraph here...</p>
-      </div>
+    <div>
+      <div className="flex justify-between pt-[18px]">
+        <div className="w-2/3">
+          <h1 className="text-3xl font-bold mb-4 pl-2">{post.title}</h1>
+          <img
+            src={`https://picsum.photos/seed/${post.id}/300/200`}
+            alt={post.title}
+            className="w-full h-40 object-cover rounded float-left p-1.5 mb-4"
+          />
+          <p className="text-justify pl-2">Your paragraph here...</p>
+        </div>
 
-      <div className="w-1/3 mt-8 pl-3">
-        <span className="text-xl font-semibold pl-5">Latest Posts</span>
-        <ul className="text-sm space-y-2 p-4 rounded-lg">
-          {[...Array(5)].map((_, idx) => (
-            <li
-              key={idx}
-              className="flex items-center bg-blue-400 text-white rounded px-4 py-2 [border-top-right-radius:222px] [border-bottom-right-radius:222px]"
-            >
-              <div className="h-3 w-3 rounded-full bg-white"></div>
-              <span className="pl-3">
-                <Link href={`/gallery/${post.id}`}>{post.title}</Link>
-              </span>
-            </li>
-          ))}
-        </ul>
+        <div className="w-1/3 mt-8 pl-3">
+          <span className="text-xl font-semibold pl-5">Latest Posts</span>
+          <ul className="text-sm space-y-2 p-4 rounded-lg">
+            {[...Array(5)].map((_, idx) => (
+              <li
+                key={idx}
+                className="flex items-center bg-blue-400 text-white rounded px-4 py-2 [border-top-right-radius:222px] [border-bottom-right-radius:222px]"
+              >
+                <div className="h-3 w-3 rounded-full bg-white"></div>
+                <span className="pl-3">
+                  <Link href={`/gallery/${post.id}`}>{post.title}</Link>
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   )
